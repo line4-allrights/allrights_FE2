51b65ec2508468956ddd52565488cf2ef1a35940
kickcha <chasm030517@gmail.com>
2023-11-09 18:13:41 +0900

    🎨 배경음악 메뉴바 제작 중

--- src/pages/bgmusic/style.jsx
+++ src/pages/bgmusic/style.jsx
@@ -14,6 +14,7 @@ export const SearchInput = styled.input`
   border: 2px solid #bfc5d0;
   margin-right: 1.5rem;
   outline: none;
+  font-size: 0.8vw;
   color: #e4e8ef;
   /* border: 1px solid red; */
 `;
@@ -46,6 +47,26 @@ export const BgMenuContainer = styled.div`
   width: 100%;
   border-bottom: 1px solid #727782;
   justify-content: center;
+  position: relative;
+
+  /* border: 1px solid red; */
+`;
+
+export const SideBarWrapper = styled.div`
+  display: flex;
+  position: relative;
+  width: 100%;
+  justify-content: center;
+`;
+
+export const SideBarContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  /* z-index: 9999; */
+
+  border: 1px solid yellow;
 `;
 
 export const BgMenubar = styled.div`
@@ -56,7 +77,20 @@ export const BgMenubar = styled.div`
   justify-content: space-between;
   color: #e4e8ef;
   top: -0.5vw;
-  /* border: 3px solid yellow; */
+  /* border: 3px solid lightblue; */
+`;
+
+// 추가: 사이드바가 열릴 때 나타날 박스 스타일
+export const ExpandedBox = styled.div`
+  width: 100%; // 화면 너비의 67vw로 설정
+  height: 31vw;
+  background-color: #bfc5d0;
+  position: absolute;
+  top: 100%; /* SideBarWrapper 아래에 위치 */
+  right: 0;
+  display: ${({ isOpen }) => (isOpen ? "block" : "none")};
+  z-index: 99999;
+  /* border: 1px solid lightgreen; */
 `;
 
 export const BgMenu = styled.div`
@@ -66,6 +100,17 @@ export const BgMenu = styled.div`
   font-weight: 400;
   line-height: 150%;
   letter-spacing: -0.8px;
+
+  ${(props) =>
+    props.active &&
+    `
+    color: #E4E8EF;
+    text-emphasis: filled #e4e8ef;
+  `}
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 export const BgBox = styled.div`
@@ -114,6 +159,7 @@ export const BgIntro2 = styled.div`
 export const BgImg = styled.img`
   display: flex;
   width: 40vw;
+  height: 28vw;
 `;
 
 export const ButtonBox = styled.div`
