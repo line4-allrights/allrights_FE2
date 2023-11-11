0f375509a359ca088c9a318e377ab0a9bbccabcd
kickcha <chasm030517@gmail.com>
2023-11-11 13:23:34 +0900

    ✨ 효과음 배경음악

--- src/components/musiclist/Musiclist.jsx
+++ src/components/musiclist/Musiclist.jsx
@@ -1,3 +1,4 @@
+// Musiclist.jsx
 import React, { useState, useEffect } from "react";
 import * as S from "./style";
 import styled from "styled-components";
@@ -26,7 +27,12 @@ const Musiclist = () => {
     const fetchMusicData = async () => {
       try {
         const response = await API.get("http://127.0.0.1:8000/music/");
-        setMusicData(response.data);
+        // Combine arrays from different properties
+        const combinedMusicData = [
+          ...response.data.effect_music,
+          ...response.data.bg_music,
+        ];
+        setMusicData(combinedMusicData);
       } catch (error) {
         console.error("음악 데이터를 가져오는 중 에러 발생:", error);
       }
@@ -35,75 +41,72 @@ const Musiclist = () => {
     fetchMusicData();
   }, []);
 
-  if (!musicData || musicData.length === 0) {
-    return <div></div>;
+  if (!musicData) {
+    return <div>Loading...</div>;
   }
+
+  if (!Array.isArray(musicData)) {
+    console.error("Music data is not an array:", musicData);
+    return <div>Error loading music data</div>;
+  }
+
+  if (musicData.length === 0) {
+    return <div>No music data available</div>;
+  }
+
+  const handleHeartClick = async (id) => {
+    try {
+      const response = await API.post(
+        `http://127.0.0.1:8000/music/like/${id}/`,
+        null,
+        {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk5NjMwODM1LCJpYXQiOjE2OTk2MzA1MzUsImp0aSI6IjA2NThkNjBjNDE5YjQ3NzM4NGMzNjJlN2Y5ODk2ZjE4IiwidXNlcl9pZCI6NX0.X_IeTKXA61woFwp_4BBh8lnxCxfi2Ta7CvVHARu0qRw",
+          },
+        }
+      );
+      console.log("좋아요 토글 성공:", response.data);
+      setIsSaved(!isSaved);
+    } catch (error) {
+      console.error("좋아요 토글 실패:", error);
+    }
+  };
+  const handleButtonClick = async (id, title) => {
+    try {
+      const response = await API.get(
+        `http://127.0.0.1:8000/music/download/${id}/`,
+        {
+          responseType: "blob",
+        }
+      );
+
+      const downloadUrl = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement("a");
+      link.href = downloadUrl;
+      link.setAttribute("download", `${title}.mp3`);
+      document.body.appendChild(link);
+      link.click();
+
+      window.URL.revokeObjectURL(downloadUrl);
+      document.body.removeChild(link);
+    } catch (error) {
+      console.error("다운로드 실패", error);
+    }
+  };
 
   return (
     <>
       {musicData.map((music) => {
         const { id, username, title, music_image } = music;
-        //좋아요 스크랩 기능
-        const toggleLike = async (musicId) => {
-          try {
-            const response = await API.post(
-              `http://127.0.0.1:8000/music/like/${musicId}/`
-            );
-
-            console.log("좋아요 토글 성공:", response.data);
-            return response.data;
-          } catch (error) {
-            console.error("좋아요 토글 실패:", error);
-            throw error;
-          }
-        };
-
-        const getHeartColor = () => (isSaved ? "#4A88FF" : "#727782");
-
-        const handleHeartClick = async () => {
-          try {
-            await toggleLike(id);
-            setIsSaved(!isSaved);
-          } catch (error) {
-            console.error("좋아요 실패", error);
-          }
-        };
-
-        const handleButtonClick = async () => {
-          try {
-            // 다운로드 요청 보내기
-            const response = await API.get(
-              `http://127.0.0.1:8000/music/download/${id}/`,
-              {
-                responseType: "blob",
-              }
-            );
-
-            // 파일 다운로드 링크 생성
-            const downloadUrl = window.URL.createObjectURL(
-              new Blob([response.data])
-            );
-            const link = document.createElement("a");
-            link.href = downloadUrl;
-            link.setAttribute("download", `${title}.mp3`);
-            document.body.appendChild(link);
-            link.click();
-
-            // 다운로드 후 링크 및 객체 제거
-            window.URL.revokeObjectURL(downloadUrl);
-            document.body.removeChild(link);
-          } catch (error) {
-            console.error("다운로드 실패", error);
-          }
-        };
 
         return (
           <S.MusicBox key={id}>
             <S.MusicSource>
               <FontAwesomeIcon
                 icon={faHeart}
-                onClick={handleHeartClick}
-                style={{ color: getHeartColor() }}
+                onClick={() => handleHeartClick(id)}
+                style={{ color: isSaved ? "#4A88FF" : "#727782" }}
               />
               <AlbumCover imageUrl={music_image} />
               <S.MusicInfo>
@@ -111,7 +114,7 @@ const Musiclist = () => {
                 <S.MusicOwner>{username}</S.MusicOwner>
               </S.MusicInfo>
               <AudioWaveform musicData={music} />
-              <S.DownloadBtn onClick={handleButtonClick}>
+              <S.DownloadBtn onClick={() => handleButtonClick(id, title)}>
                 다운로드
               </S.DownloadBtn>
             </S.MusicSource>
